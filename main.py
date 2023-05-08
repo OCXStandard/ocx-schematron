914a4af0af1491567549ef191cd52d984621a6da
ocastrup <ole.christain.astrup@dnv.com>
2023-05-08 08:41:09 +0200

    Add Jetbrains ignore and Schematron test files

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #  Copyright (c) 2023. OCX Consortium https://3docx.org. See the LICENSE
 
 import typer
-
+from pathlib import Path
+from ocx_schematron.validate import OcxValidator
 def main(name: str):
     print(f"Hello {name}")
 
+def validate(model: Path, rule:Path):
+    validator = OcxValidator()
+    validator.validate(model, rule)
+
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(validate)
--- ocx_schematron/validate.py
+++ ocx_schematron/validate.py
@@ -1,2 +1,52 @@
 #  Copyright (c) 2023. OCX Consortium https://3docx.org. See the LICENSE
+"""Validate an XML file with a Schematron schema."""
+from lxml import etree
+from lxml.etree import XMLSyntaxError
+from pathlib import Path
 
+
+class OcxValidator:
+    def __init__(self):
+        self.__rule: Path = 'schematron.sch'
+        self.__model: Path = ''
+
+    def assign_rule(self, schema: Path) -> bool:
+        self.__rule= schema
+        return self.__rule.exists()
+
+    def assign_model(self, model: Path) -> bool:
+        self.__model = model
+        return self.__model.exists()
+
+    def validate(self, model: Path, rule: Path) -> bool:
+        """Validate the model."""
+        # Parse the XML file to be validated
+        if self.assign_model(model):
+            try:
+                xml_doc  = etree.parse(self.__model)
+            except XMLSyntaxError as e:
+                print(e)
+                return False
+        else:
+            print(f'(The OCX model {model} does not exist')
+            return False
+        if self.assign_rule(rule):
+            # Parse the Schematron schema
+            try:
+                schematron_doc = etree.parse(self.__rule)
+            except XMLSyntaxError as e:
+                print(e)
+                return False
+        else:
+            print(f'(The Schematron schema {rule} does not exist')
+            return False
+        # Create the Schematron validator
+        schematron = etree.Schematron(schematron_doc)
+        # Validate the XML file against the Schematron schema
+        validation_result = schematron.validate(xml_doc)
+        if validation_result:
+            # Print the validation result
+            print(validation_result)
+            return True
+        else:
+            assert (validation_result)
